c091eacf5dc2348076a72f77e3480072b787afbf
lixiaoxu <lixiaoxu@shinetechchina.com>
2022-03-15 16:11:21 +0800

    commit

--- routes/web.php
+++ routes/web.php
@@ -20,4 +20,4 @@ Route::any('api/deleteCartItem',        'CartController@deleteCartItem');
 Route::any('api/updateCartQty',         'CartController@updateCartQty');
 Route::any('api/addCart',               'CartController@addCart');
 
-Route::any('api/getAddressList',        'UserController@addCart');
+Route::any('api/getAddressList',        'UserController@getAddressList');
